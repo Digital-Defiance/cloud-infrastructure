ad96809654a9ab19272b6594ba982146dcd9b53a
ruicampos <mail@ruicampos.org>
2024-08-26 08:41:57 +0100

    fix: .tf syntax error in user data

--- k8s/eks.tf
+++ k8s/eks.tf
@@ -209,7 +209,7 @@ docker version
 
 # INSTALL NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+export NVM_DIR="$([ -z "$\{XDG_CONFIG_HOME-}" ] && printf %s "$\{HOME}/.nvm" || printf %s "$\{XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 # INSTALL NODE 20.16.0

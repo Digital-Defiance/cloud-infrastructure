f9e276e93f2ef6e607cd3d85bda9758885876c81
ruicampos <mail@ruicampos.org>
2024-08-26 08:39:09 +0100

    fix: commands now use the npm installed devcontainer cli
    vscode devcontainer extension installs a modified version of the devcontainer cli, they behave differently even though they print the the same version number

--- k8s/eks.tf
+++ k8s/eks.tf
@@ -1,5 +1,4 @@
 terraform {
-
   backend "s3" {
     key    = "cloud-infrastructure"
     bucket = "digitaldefiance-terraform-backend"
@@ -199,19 +198,30 @@ module "ec2_instance" {
   source    = "terraform-aws-modules/ec2-instance/aws"
   user_data = <<EOF
 #!/bin/bash
+
+# INSTALL DOCKER
 sudo apt update -y
 sudo apt -y install docker.io
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo chmod 666 /var/run/docker.sock
 docker version
+
+# INSTALL NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-nvm install 20
-nvm use 20
-npm install -g @devcontainers/cli
+
+# INSTALL NODE 20.16.0
+nvm install 20.16.0
+nvm use 20.16.0
+
+# INSTALL DEVCONTAINERS CLI 0.65.0
+npm install -g @devcontainers/cli@0.65.0
+
+# INSTALL MAKE AND SETUP DEV CONTAINER
 sudo apt install make
+cd $HOME
 git clone https://github.com/Digital-Defiance/cloud-infrastructure.git
 cd cloud-infrastructure
 make build

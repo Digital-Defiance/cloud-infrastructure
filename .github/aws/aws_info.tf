f6c03eb39c8c36b73d877482916a9cea0cb05821
ruicampos <mail@ruicampos.org>
2024-08-27 16:48:26 +0100

    fix(cicd): add ami info

--- .github/aws/aws_info.tf
+++ .github/aws/aws_info.tf
@@ -45,6 +45,30 @@ data "aws_subnets" "selected" {
   }
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical's AWS account ID
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+output "ami" {
+  value = data.aws_ami.ubuntu.id
+}
+
 output "subnet_ids" {
   value = data.aws_subnets.selected
 }

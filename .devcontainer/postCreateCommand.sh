a3e3a4da76944d3acbb2cb0bb08889ec48820d45
Rui <mail@ruicampos.org>
2024-08-28 18:07:54 +0100

    feat: add fzf in postcreatecommand

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,7 @@
 git clone https://github.com/RuiFilipeCampos/nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+$HOME/.fzf/install --key-bindings --completion --update-rc
+echo "alias nf='nvim \$(fzf)'" >> $HOME/.bashrc
 echo "source /workspaces/cloud-infrastructure/.1password" >> $HOME/.bashrc
 echo "eval \$(op signin)" >> $HOME/.bashrc
 echo "op run -- gh auth setup-git" >> $HOME/.bashrc

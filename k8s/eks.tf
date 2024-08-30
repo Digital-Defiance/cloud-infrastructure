09446f64a64ad291dfbe9afe1cad1a3fe6603556
Rui <mail@ruicampos.org>
2024-08-30 13:14:22 +0100

    fix: vpc db subnets

--- k8s/eks.tf
+++ k8s/eks.tf
@@ -43,18 +43,20 @@ data "aws_availability_zones" "available" {
   }
 }
 
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.13.0"
 
-  name = "cloud-dev-infra"
+  name = "vpc-cloud-dev-infra"
 
   cidr = "10.0.0.0/16"
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
 
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  private_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets   = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  database_subnets = ["10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
+
+  database_subnet_group_name = "rds_subnet_group"
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
@@ -97,7 +99,9 @@ data "aws_rds_orderable_db_instance" "selected" {
   preferred_instance_classes = ["db.t3.micro"]
 }
 
-
+output "vpc" {
+  value = module.vpc
+}
 module "db" {
   source = "terraform-aws-modules/rds/aws"
 

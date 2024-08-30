cca1e7b2437f7a3d32da45fad024a9071256bd28
Rui <mail@ruicampos.org>
2024-08-30 14:50:41 +0100

    fix: add ip ranges to in/out bound trafick

--- k8s/eks.tf
+++ k8s/eks.tf
@@ -103,13 +103,17 @@ module "postgresql_security_group" {
   source  = "terraform-aws-modules/security-group/aws//modules/postgresql"
   version = "~> 5.0"
   name    = "postgresql_security_group"
-  vpc_id  = module.vpc.id
+  vpc_id  = module.vpc.vpc_id
+
+
+  egress_cidr_blocks  = concat(module.vpc.private_subnets_cidr_blocks, module.vpc.public_subnets_cidr_blocks)
+  ingress_cidr_blocks = concat(module.vpc.private_subnets_cidr_blocks, module.vpc.public_subnets_cidr_blocks)
 }
 
 module "db" {
   source = "terraform-aws-modules/rds/aws"
 
-  apply_immediately = true
+  apply_immediately  = true
   create_db_instance = true
 
   db_subnet_group_name = module.vpc.database_subnet_group_name
